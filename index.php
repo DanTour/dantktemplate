fdf34bf98e9f9eee32b226478a22a9d6b5eec3b4
manoshkin-dv <manoshkin-dv@dantk.ru>
2016-02-19 17:06:35 +0500

    template dantk footer menu

--- index.php
+++ index.php
@@ -41,16 +41,13 @@ require dirname(__FILE__) . '/php/init.php';
 							<span class="icon-bar"></span>
 						</button>
 						<a class="navbar-brand" href="/">
-								<!--<img class="visible-lg" src="templates/jblank/images/logos/logo-original.png" alt="Туристическая компания ДАН">
-								<img class="visible-md" src="templates/jblank/images/logos/logo_sm.png" alt="Туристическая компания ДАН">
-								<img class="visible-sm" src="templates/jblank/images/logos/logo_xs.png" alt="Туристическая компания ДАН">-->
-							</a>
+						</a>
 					</div>
 						
 					<div class="collapse navbar-collapse col-md-9 col-lg-9 no-gutter" id="bs-example-navbar-collapse-1">
 						<div class="phones-main visible-lg">
-							8-351-211-38-38<br>
-							8-800-300-76-50
+							<a href="tel:+73512113838">8-351-211-38-38</a>
+							<a href="tel:+78003007650">8-800-300-76-50</a>
 						</div>
 						<jdoc:include type="modules" name="main_menu" />
 					</div>
@@ -61,16 +58,29 @@ require dirname(__FILE__) . '/php/init.php';
 		<div class="row">
 			<div class="col-md-3 col-lg-3 hidden-xs hidden-sm">
 				<div class="triangle-wrapper clearfix">
-					<jdoc:include type="modules" name="triangle_menu" />
-					<div class="triangle-block"></div>
+					<div class="wrapper clearfix">
+						<jdoc:include type="modules" name="triangle_menu" />
+					</div>
+					<div class="tr-social-buttons">
+						<div class="soc-buttons">
+							<a class= "tr-vk" href="http://vk.com/dantk" title="Туристическая компания Дан Вконтакте"></a>
+							<a class="tr-twitter" href="https://twitter.com/dan_chel" title="Туристическая компания Дан в Twitter"></a>
+							<a class="tr-youtube" href="http://www.youtube.com/channel/UCK0gnncvShhU1pJkUCkPgOA" title="Туристическая компания Дан Youtube"></a>
+							<a class="tr-facebook" href="https://www.facebook.com/dancheltours" title="Туристическая компания Дан facebook"></a>
+							<a class="tr-instagram" href="https://www.instagram.com/dantk.ru/" titile="Туристическая компания Дан Instagram"></a>
+						</div>
+					</div>
 				</div>
 			</div>
-				<div class="col-md-9 col-lg-3 hidden-xs hidden-sm">
+			<div class="col-md-9 col-lg-3 hidden-xs hidden-sm">
+				
 			</div>
 		</div>
 	</div>
 					
-	<jdoc:include type="modules" name="header_carousel" />
+	<div class="carousel-imgs hidden-xs hidden-sm">
+		<jdoc:include type="modules" name="header_carousel" />
+	</div>
 </header>	
 		
 		
@@ -97,13 +107,107 @@ require dirname(__FILE__) . '/php/init.php';
          <jdoc:include type="component" />
      
    <footer>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-9 col-sm-12">
-			
+	<div class="container-fluid no-gutter">
+		<div class="footer-top">
+			<div class="row">
+				<div class="col-md-9 col-sm-12">
+				  <jdoc:include type="modules" name="footer_menu" />
+					<!--<div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingOne">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordion2" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne" class="collapsed">
+										Collapsible Group Item #1
+									</a>
+								</h4>
+							</div>
+							<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne" aria-expanded="false" style="height: 0px;">
+								<div class="panel-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat 
+									skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+									single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+									Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you 
+									probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingTwo">
+								<h4 class="panel-title">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										Collapsible Group Item #2
+									</a>
+								</h4>
+							</div>
+							<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo" aria-expanded="false">
+								<div class="panel-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat 
+									skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+									coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+									Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven'tab
+									heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingThree">
+								<h4 class="panel-title">
+									<a class="collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										Collapsible Group Item #3
+									</a>
+								</h4>
+							</div>
+							<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree" aria-expanded="false">
+								<div class="panel-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard 
+									dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird 
+									on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes 
+									anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer 
+									farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+					</div>-->
 			</div>
-			<div class="col-md-9 col-sm-12">
-			
+				<div class="col-md-3 col-sm-12">
+					<div class="footer-contacts">
+						<h4>контакты</h4>
+						<div class="phones hidden-sm hidden-xs">
+							<ul>
+								<li><a href="tel:+73512113838">8-351-211-38-38</a></li>
+								<li><a href="tel:+78003007650">8-800-300-76-50</a></li>
+							</ul>
+						</div>
+						<div class="adress">
+							<ul>
+								<li>Г. ЧЕЛЯБИНСК, ПРОСПЕКТ ЛЕНИНА, Д.30</li>
+								<li><span>Телефон:</span> <a href="tel:+73512113838">8-351-211-38-38</a></li>
+								<li><span>Электронная почта:</span> <a href="mailto:info@dan-chel.ru">info@dan-chel.ru</a></li>
+							</ul>
+						</div>
+						<div class="adress">
+							<ul>
+								<li>Г. ЧЕЛЯБИНСК, ПР. КОМСОМОЛЬСКИЙ , Д.94</li>
+								<li><span>Телефон:</span> <a href="tel:+73517349834">8-351-734-98-34</a></li>
+								<li><span>Электронная почта:</span> <a href="mailto:info@dan-chel.ru">info@dan-chel.ru</a></li>
+							</ul>
+						</div>
+						<div class="emails">
+							<ul>
+								<li><span>Тур. отдел: <span><a href="mailto:mpp@dan-chel.ru">mpp@dan-chel.ru</a></li>
+								<li><span>Агент. отдел: </span><a href="mailto:info@dan-chel.ru">info@dan-chel.ru</a></li>
+								<li><span>Бух. отдел: </span><a href="mailto:oplata@dan-chel.ru">oplata@dan-chel.ru</a></li>
+								<li><span>Отдел маркетинга: </span> <a href="mailto:market@dan-chel.ru">market@dan-chel.ru</a></li>
+							</ul>
+						</div>
+					</div>				
+				</div>
+			</div>
+		</div>
+		<div class="footer-bottom clearfix">
+			<div class="row ">
+				<a href="/" titile="Туристическая компания Дан"></a>
+				<p>© Туристическая компания ДАН, 1996 - <?php echo date('Y') ?> </p>
 			</div>
 		</div>
 	</div>
